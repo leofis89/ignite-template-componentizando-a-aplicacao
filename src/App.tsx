4af0671c0207042387e79bd4b8cfc5ee3615d33c
Leonardo Leite <jonyleonardo89@gmail.com>
2021-04-02 10:17:15 -0300

    Refact: tirando imports + import content

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,46 @@
-import { useEffect, useState } from 'react';
+	import { useState } from 'react';
+	import  SideBar  from './components/SideBar';
+	import  Content  from './components/Content';
+	import './styles/global.scss';
+	import './styles/sidebar.scss';
+	import './styles/content.scss';
 
-import { Button } from './components/Button';
-import { MovieCard } from './components/MovieCard';
+	interface GenreResponseProps {
+	id: number;
+	name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+	title: string;
+	}
 
-import  SideBar  from './components/SideBar';
-// import { Content } from './components/Content';
+	interface MovieProps {
+	imdbID: string;
+	Title: string;
+	Poster: string;
+	Ratings: Array<{
+		Source: string;
+		Value: string;
+	}>;
+	Runtime: string;
+	}
 
-import { api } from './services/api';
+	export function App () {
+	
+	const [selectedGenreId, setSelectedGenreId] = useState(1);
 
-import './styles/global.scss';
-
-import './styles/sidebar.scss';
-import './styles/content.scss';
-
-interface GenreResponseProps {
-  id: number;
-  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
-  title: string;
-}
-
-interface MovieProps {
-  imdbID: string;
-  Title: string;
-  Poster: string;
-  Ratings: Array<{
-    Source: string;
-    Value: string;
-  }>;
-  Runtime: string;
-}
-
-export function App () {
-  
-  const [selectedGenreId, setSelectedGenreId] = useState(1);
-
-  function handleClickButton(id: number) {
-    setSelectedGenreId(id);
-  }
+	function handleClickButton(id: number) {
+		setSelectedGenreId(id);
+	}
 
 
 
-  return (
-    <div style={{ display: 'flex', flexDirection: 'row' }}>
-     
-     <SideBar onClickButton={handleClickButton} selected={selectedGenreId} />
+	return (
+		<div style={{ display: 'flex', flexDirection: 'row' }}>
+		
+		<SideBar onClickButton={handleClickButton} selected={selectedGenreId} />
 
-      <div className="container">
-
-        {/* MAIN */}
-        <header>
-          <span className="category">Categoria:<span> {selectedGenre.title}</span></span>
-        </header>
-
-        <main>
-          <div className="movies-list">
-            {movies.map(movie => (
-              <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
-            ))}
-          </div>
-        </main>
-      </div>
-    </div>
-  )
-}
+		<div className="container">
+			<Content />
+		
+		</div>
+		</div>
+	)
+	}
