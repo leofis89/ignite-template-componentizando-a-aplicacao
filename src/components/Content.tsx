2ffd92030c70807f68b1ceb1aeef40eaca32a9d7
Leonardo Leite <jonyleonardo89@gmail.com>
2021-04-02 10:15:18 -0300

    Feat: Comp content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,3 +1,58 @@
-export function Content() {
-  // Complete aqui
+import { useState, useEffect } from "react";
+import { api } from '../services/api';
+import { MovieCard } from '../components/MovieCard';
+
+interface Content {
+	selected: number;
 }
+interface GenreResponseProps {
+	id: number;
+	name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+	title: string;
+}
+
+interface MovieProps {
+	Title: string;
+	Poster: string;
+	Ratings: Array<{
+	Source: string;
+	Value: string;
+	}>;
+	Runtime: string;
+}
+
+
+
+export default function Content (props: Content) {
+	
+	const [movies, setMovies] = useState<MovieProps[]>([]);
+	const [selectedGenre, setSelectedGenre] = useState<GenreResponseProps>({} as GenreResponseProps);
+
+	useEffect(() => {
+		api.get<MovieProps[]>(`movies/?Genre_id=${props.selected}`).then(response => {
+		setMovies(response.data);
+	});
+
+		api.get<GenreResponseProps>(`genres/${props.selected}`).then(response => {
+		setSelectedGenre(response.data);
+	})
+	}, [props.selected]);
+
+
+	return (
+		<>
+			<header>
+				<span className="category">Categoria:<span> { selectedGenre.title }</span>
+				</span>
+			</header>
+
+			<main>
+				<div className="movies-list">
+					{movies.map(movie => (
+					<MovieCard key ={movie.Title} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+					))}
+				</div>
+			</main>
+		</>
+	)
+	}
